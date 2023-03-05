4550b1e6380ddf54160844e837c3f91d1867cc14
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 21:52:33 +0100

    generate tables of 10.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=1, N=10;
+        while(i<=10){
+            System.out.println(N + "*" + i + "=" + N*i);
+            i++;
+        }
+    }
+}
